bbf422dd91a2cd5b9bf9c07ceaf9d94c159372f5
KangTaeJong98 <rkdxowhd98@naver.com>
2021-03-16 01:47:24 +0900

    QR Code First Commit

---
+++ Android/QRCode/app/src/main/java/com/taetae98/qrcode/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taetae98.qrcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
